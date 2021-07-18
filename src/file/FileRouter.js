1df62a68b046591da900812342094b35e7bc1f9a
KoKaung <kaungyeehein@gmail.com>
2021-07-18 10:55:34 +0630

    file size limit

--- src/file/FileRouter.js
+++ src/file/FileRouter.js
@@ -2,11 +2,19 @@ const router = require('express').Router();
 const multer = require('multer');
 const fs = require('fs');
 
-const upload = multer();
+const upload = multer({
+  limits: { fileSize: 1024 }
+}).single('my-file');
 
-router.post('/upload', upload.single('my-file'), async (req, res) => {
-  await fs.promises.writeFile('./static/' + req.file.originalname, req.file.buffer);
-  res.send();
+router.post('/upload', async (req, res) => {
+  upload(req, res, async (error) => {
+    if (error) {
+      return res.status(400).send({ message: "Size is too big" });
+    }
+    await fs.promises.writeFile('./static/' + req.file.originalname, req.file.buffer);
+    res.send({ message: "Upload success" });
+  });
+
 });
 
 module.exports = router;
